5228070cadc82e48fe071c0c6a479dd81917ed4c
boy-stack <85478416+boy-satck@users.noreply.github.com>
2025-02-11 19:37:09 +0700

    update assets

---
+++ public/assets.js
@@ -0,0 +1,210 @@
+import blog_pic_1 from "./blog_pic_1.png";
+
+import blog_pic_3 from "./blog_pic_3.png";
+import blog_pic_4 from "./blog_pic_4.png";
+import blog_pic_5 from "./blog_pic_5.png";
+
+import blog_pic_7 from "./blog_pic_7.png";
+import blog_pic_8 from "./blog_pic_8.png";
+
+import blog_pic_10 from "./blog_pic_10.png";
+
+import blog_pic_12 from "./blog_pic_12.png";
+
+import blog_pic_14 from "./blog_pic_14.png";
+import blog_pic_15 from "./blog_pic_15.png";
+
+import blog_pic_17 from "./blog_pic_17.png";
+import blog_pic_18 from "./blog_pic_18.png";
+import blog_pic_19 from "./blog_pic_19.png";
+import blog_pic_20 from "./blog_pic_20.png";
+import facebook_icon from "./facebook_icon.png";
+import googleplus_icon from "./googleplus_icon.png";
+import twitter_icon from "./twitter_icon.png";
+import profile_icon from "./profile_icon.png";
+import logo from "./logo.png";
+import arrow from "./arrow.png";
+import logo_light from "./logo_light.png";
+import logo_TechTrekker from "./logo_TechTrekker.png";
+import blog_icon from "./blog_icon.png";
+import add_icon from "./add_icon.png";
+import email_icon from "./email_icon.png";
+import upload_area from "./upload_area.png";
+
+export const assets = {
+  facebook_icon,
+  googleplus_icon,
+  twitter_icon,
+  profile_icon,
+  logo,
+  logo_TechTrekker,
+  arrow,
+  logo_light,
+  blog_icon,
+  add_icon,
+  email_icon,
+  upload_area,
+};
+
+export const blog_data = [
+  {
+    id: 1,
+    title: "A detailed step by step guide to manage your lifestyle",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the..",
+    image: blog_pic_1,
+    date: Date.now(),
+    category: "Lifestyle",
+    author: "Alex Bennett",
+    author_img: profile_icon,
+  },
+
+  {
+    id: 3,
+    title: "Learning new technology to boost your career in software",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the..",
+    image: blog_pic_3,
+    date: Date.now(),
+    category: "Technology",
+    author: "Alex Bennett",
+    author_img: profile_icon,
+  },
+  {
+    id: 4,
+    title: "Tips for getting the most out of apps and software",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the..",
+    image: blog_pic_4,
+    date: Date.now(),
+    category: "Technology",
+    author: "Alex Bennett",
+    author_img: profile_icon,
+  },
+  {
+    id: 5,
+    title: "Enhancing your skills and capturing memorable moments",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the..",
+    image: blog_pic_5,
+    date: Date.now(),
+    category: "Lifestyle",
+    author: "Alex Bennett",
+    author_img: profile_icon,
+  },
+
+  {
+    id: 7,
+    title: "Technology for Career advancement in development",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the..",
+    image: blog_pic_7,
+    date: Date.now(),
+    category: "Technology",
+    author: "Alex Bennett",
+    author_img: profile_icon,
+  },
+  {
+    id: 8,
+    title: "A comprehensive roadmap for effective lifestyle management",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the..",
+    image: blog_pic_8,
+    date: Date.now(),
+    category: "Lifestyle",
+    author: "Alex Bennett",
+    author_img: profile_icon,
+  },
+
+  {
+    id: 10,
+    title: "Beyond the Ordinary: Crafting Your Exceptional Lifestyle",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the..",
+    image: blog_pic_10,
+    date: Date.now(),
+    category: "Lifestyle",
+    author: "Alex Bennett",
+    author_img: profile_icon,
+  },
+
+  {
+    id: 12,
+    title: "How to design an online Learning Platform today",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the..",
+    image: blog_pic_12,
+    date: Date.now(),
+    category: "Technology",
+    author: "Alex Bennett",
+    author_img: profile_icon,
+  },
+
+  {
+    id: 14,
+    title: "Balance & Bliss: Navigating Life's Journey with Style",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the..",
+    image: blog_pic_14,
+    date: Date.now(),
+    category: "Lifestyle",
+    author: "Alex Bennett",
+    author_img: profile_icon,
+  },
+  {
+    id: 15,
+    title: "Exploring the Evolution of social networking in the Future",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the..",
+    image: blog_pic_15,
+    date: Date.now(),
+    category: "Technology",
+    author: "Alex Bennett",
+    author_img: profile_icon,
+  },
+
+  {
+    id: 17,
+    title: "My frist Unboxing Edifier BT800 Pro",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the..",
+    image: blog_pic_17,
+    date: Date.now(),
+    category: "Newsroom",
+    author: "Muhammad Shidiq",
+    author_img: profile_icon,
+  },
+  {
+    id: 18,
+    title: "My frist Unboxing Edifier BT800 Pro",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the..",
+    image: blog_pic_18,
+    date: Date.now(),
+    category: "Newsroom",
+    author: "Muhammad Shidiq",
+    author_img: profile_icon,
+  },
+  {
+    id: 19,
+    title: "My frist Unboxing onnz angle solo",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the..",
+    image: blog_pic_19,
+    date: Date.now(),
+    category: "Newsroom",
+    author: "Muhammad Shidiq",
+    author_img: profile_icon,
+  },
+  {
+    id: 20,
+    title: "My frist Unboxing dual speaker onnz angle solo",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea ratione quas minus quasi, asperiores neque numquam officiis debitis assumenda qui soluta quod. Optio officia culpa, quibusdam delectus ea suscipit veniam.",
+    image: blog_pic_20,
+    date: Date.now(),
+    category: "Wintech",
+    author: "Muhammad Shidiq",
+    author_img: profile_icon,
+  },
+];

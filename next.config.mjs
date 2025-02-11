bc353cb13e039575a3a578cbe468eb2f6ee2c910
boy-stack <85478416+boy-satck@users.noreply.github.com>
2025-02-11 15:51:37 +0700

    update config next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import { fileURLToPath } from "url";
+import path from "path";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    config.resolve.alias["@"] = path.resolve(__dirname, "assets"); // Mengarahkan alias '@' ke folder 'assets'
+    return config;
+  },
+};
 
 export default nextConfig;

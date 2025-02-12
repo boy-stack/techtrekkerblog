3400f3bbc487de07556336c5da25b7284f7f7c01
boy-stack <85478416+boy-satck@users.noreply.github.com>
2025-02-12 13:15:11 +0700

    Update

--- app/page.js
+++ app/page.js
@@ -4,14 +4,17 @@ import Header from "../Components/Header";
 import BlogList from "@/Components/BlogList";
 import Footer from "@/Components/Footer";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; // Ensure you import Toastify CSS
 
 export default function Home() {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <ToastContainer theme="dark" />
       <Header />
-      <BlogList />
+      <main className="flex-grow">
+        <BlogList />
+      </main>
       <Footer />
-    </>
+    </div>
   );
 }

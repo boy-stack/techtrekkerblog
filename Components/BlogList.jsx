bea2321f0e87a4d64008c44f18db447994566533
boy-stack <85478416+boy-satck@users.noreply.github.com>
2025-02-12 12:56:34 +0700

    Update Bloglist

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -1,12 +1,12 @@
 import { blog_data } from "@/Assets/assets";
 import React, { useEffect, useState } from "react";
 import BlogItem from "./BlogItem";
-import { set } from "mongoose";
 import axios from "axios";
 
 export const BlogList = () => {
   const [menu, setMenu] = useState("All");
   const [blogs, setBlogs] = useState([]);
+
   const fetchBlogs = async () => {
     const response = await axios.get("/api/blog");
     setBlogs(response.data.blogs);
@@ -20,66 +20,33 @@ export const BlogList = () => {
   return (
     <div>
       <div className="flex justify-center gap-6 my-10 overflow-x-auto whitespace-nowrap">
-        <button
-          onClick={() => setMenu("All")}
-          className={
-            menu == "All" ? "bg-black text-white py-1 px-4 rounded-sm" : ""
-          }
-        >
-          All
-        </button>
-        <button
-          onClick={() => setMenu("Technology")}
-          className={
-            menu == "Technology"
-              ? "bg-black text-white py-1 px-4 rounded-sm"
-              : ""
-          }
-        >
-          Technology
-        </button>
-        <button
-          onClick={() => setMenu("Lifestyle")}
-          className={
-            menu == "Lifestyle"
-              ? "bg-black text-white py-1 px-4 rounded-sm"
-              : ""
-          }
-        >
-          Lifestyle{" "}
-        </button>
-        <button
-          onClick={() => setMenu("Newsroom")}
-          className={
-            menu == "Newsroom" ? "bg-black text-white py-1 px-4 rounded-sm" : ""
-          }
-        >
-          Newsroom
-        </button>
-        <button
-          onClick={() => setMenu("Wintech")}
-          className={
-            menu == "Wintech" ? "bg-black text-white py-1 px-4 rounded-sm" : ""
-          }
-        >
-          Wintech
-        </button>
+        {["All", "Technology", "Lifestyle", "Newsroom", "Wintech"].map(
+          (category) => (
+            <button
+              key={category}
+              onClick={() => setMenu(category)}
+              className={`py-1 px-4 rounded-sm ${
+                menu === category ? "bg-black text-white" : ""
+              }`}
+            >
+              {category}
+            </button>
+          )
+        )}
       </div>
-      <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
+      <div className="flex flex-wrap justify-around gap-4 gap-y-10 mb-16 xl:mx-24">
         {blogs
           .filter((item) => (menu === "All" ? true : item.category === menu))
-          .map((item, index) => {
-            return (
-              <BlogItem
-                key={index}
-                id={item._id}
-                image={item.image}
-                title={item.title}
-                description={item.description}
-                category={item.category}
-              />
-            );
-          })}
+          .map((item, index) => (
+            <BlogItem
+              key={index}
+              id={item._id}
+              image={item.image}
+              title={item.title}
+              description={item.description}
+              category={item.category}
+            />
+          ))}
       </div>
     </div>
   );
